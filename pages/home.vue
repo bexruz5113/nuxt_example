ffaeb12a32e55cb340ae901ddd50d345484cc7f1
bexruz5113 <bexruzbahronov2000@gmail.com>
2021-05-01 23:00:38 +0500

    documention learning

--- pages/home.vue
+++ pages/home.vue
@@ -1,13 +1,37 @@
 <template>
   <div
-    class="flex flex-col md:flex-row justify-around bg-red-500 p-8 mx-auto w-full max-w-6xl"
+    class="grid grid-cols-1 md:grid-cols-4 gap-4 justify-around bg-red-500 p-8 mt-24 mx-auto w-full max-w-7xl"
   >
     <div
-      v-for="i in 10"
-      :key="i"
-      class="flex flex-row p-8 m-5 items-center bg-yellow-500"
+      class="col-star-1 col-span-2 row-start-1 row-span-4 bg-blue-800 text-white rounded-md shadow-lg text-center"
     >
-      <span class="text-sm md:text-2xl text-white md:text-black">{{ i }}</span>
+      1
+    </div>
+    <div class="bg-green-800 text-white rounded-md shadow-lg text-center">
+      2
+    </div>
+    <div class="bg-green-800 text-white rounded-md shadow-lg text-center">
+      3
+    </div>
+    <div class="bg-green-800 text-white rounded-md shadow-lg text-center">
+      4
+    </div>
+    <div class="bg-green-800 text-white rounded-md shadow-lg text-center">
+      5
+    </div>
+    <div class="bg-green-800 text-white rounded-md shadow-lg text-center">
+      6
+    </div>
+    <div class="bg-green-800 text-white rounded-md shadow-lg text-center">
+      7
+    </div>
+    <div class="bg-green-800 text-white rounded-md shadow-lg text-center">
+      8
+    </div>
+    <div class="text-2xl">
+        <span class="bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
+          Hello world
+        </span>
     </div>
   </div>
 </template>
